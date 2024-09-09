d218aa4e0860c53e854c2fbcb8235737f3b1351a
renenyah <nyahshops@gmail.com>
2024-09-08 21:37:46 -0400

    Update CardPlayer.java

--- CardPlayer.java
+++ CardPlayer.java
@@ -1,36 +1,11 @@
 /*
  * This class represents a card player.
- *
- * For each card player instance, we should keep track of how many points
+ * For each card player instance, we keep track of how many points
  * they earned in the game so far, as well as whether it is their turn or not.
- * Additionally, their hand and bank of cards should be stored in two
+ * Additionally, their hand and bank of cards are stored in two
  * separate ArrayLists of Card objects.
  *
- * <p>
- * A player's points, turn, and hand of cards should all be declared
- * private fields, whereas the bank of cards should be public, as follows:
- * <p>
- * <code>
- * 		private int points;
- *
- * 		private boolean turn;
- *
- * 		private ArrayList&lt;Card&gt; hand = new ArrayList&lt;Card&gt;();
- *
- * 		public ArrayList&lt;Card&gt; bank = new ArrayList&lt;Card&gt;();
- * </code>
- * <p>
- *
- * Note that the Field Summary section below will only show you public fields,
- * but you must declare all the fields described above in your implementation of this class,
- * including the private fields. You are free to create additional fields if deemed necessary.
- *
- * @param <Card> the type of card used in the game
  */ import java.util.ArrayList;
-
-
-// class documentation described here:
-// https://www.cs.emory.edu/~nelsay2/cs171_s23/a2/doc/cs171a2/CardPlayer.html
 
 
 public class CardPlayer extends GeneralPlayer<Card>{ 
